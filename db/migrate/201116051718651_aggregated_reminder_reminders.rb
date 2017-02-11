935b72b6fb0199494702c5a48b0e0b8158e206d0
kchizi <samuel@medullatechnologies.com>
2017-02-11 22:04:07 +0300

    sdt

--- db/migrate/201116051718651_aggregated_reminder_reminders.rb
+++ db/migrate/201116051718651_aggregated_reminder_reminders.rb
@@ -21,7 +21,7 @@
 require Rails.root.join('db', 'migrate', 'migration_utils', 'migration_squasher').to_s
 require 'open_project/plugins/migration_mapping'
 # This migration aggregates the migrations detailed in MIGRATION_FILES
-class AggregatedReminderMigrations < ActiveRecord::Migration
+class AggregatedReminderReminders < ActiveRecord::Migration
   MIGRATION_FILES = <<-MIGRATIONS
     20110106210555_create_reminders.rb
     20110106221214_create_reminder_contents.rb
